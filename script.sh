6af3df1b4a04684cc22ec811040858cdd9ac8bf6
abhipraydhoble <122669982+abhipraydhoble@users.noreply.github.com>
2026-05-22 10:43:20 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,42 +1,42 @@
 #!/bin/bash
 
-# Update and install Java
 sudo apt update -y
-sudo apt install fontconfig openjdk-21-jre -y
+sudo apt install -y curl unzip
 
-# Add Jenkins key and source list
-sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
-echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
-
-# Update and install Jenkins
-sudo apt-get update -y
-sudo apt-get install jenkins -y
-
-# Enable and start Jenkins
-sudo systemctl enable jenkins
-sudo systemctl start jenkins
-
-# Install Docker
-sudo apt update -y
-sudo apt install docker.io -y
-sudo systemctl enable docker
-sudo systemctl start docker
-
-# Add Jenkins and Ubuntu user to Docker group
-sudo usermod -aG docker jenkins
-sudo usermod -aG docker ubuntu
-newgrp docker
-
-# Fix permissions
-sudo chmod 777 /var/run/docker.sock
-
-# aws cli 
-sudo apt install unzip -y
+# Install AWS CLI
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
 
-# install terraform
-wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update && sudo apt install terraform -y
+# Install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+# Install eksctl
+curl --silent --location \
+"https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" \
+| tar xz -C /tmp
+
+sudo mv /tmp/eksctl /usr/local/bin
+sudo chmod +x /usr/local/bin/eksctl
+
+sudo apt update -y
+sudo apt install docker.io  -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker ubuntu
+newgrp docker
+sudo chmod 777 /var/run/docker.sock
+docker --version
+
+#install jenkins 
+sudo apt update -y
+sudo apt install fontconfig openjdk-21-jre -y
+
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update
+sudo apt install jenkins -y
